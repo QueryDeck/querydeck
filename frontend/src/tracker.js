6db0850b40004425298ad41cd363c6ecc8c3bb42
Amol <amol@marquee-equity.com>
2024-12-24 15:37:27 +0530

    QC-461

--- frontend/src/tracker.js
+++ frontend/src/tracker.js
@@ -1,7 +1,7 @@
 import OpenReplay from '@openreplay/tracker'
 
 const config = {
-    projectKey: '',
+    projectKey: process.env.REACT_APP_OPENREPLAY_KEY,
     ingestPoint: 'https://openreplay.querycharts.com/ingest',
     defaultInputMode: 0,
     network: {
@@ -20,18 +20,6 @@ const config = {
     obscureTextEmails: false,
     obscureTextNumbers: false,
     verbose: true
-}
-
-switch (window.location.hostname) {
-    case 'app.querydeck.io':
-        config.projectKey = 'A69rf4P72IN4PvQ3837u'
-        break
-    case 'dev.querydeck.io':
-        config.projectKey = 'bDs7tHOXaczTcTdZuLVf'
-        break
-    default:
-        console.log('Tracker not initialised', window.location.hostname)
-        break
 }
 
 const tracker = new OpenReplay(config)
