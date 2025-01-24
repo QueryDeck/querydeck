67c2ccc22097631a6d13ab96dda08e577ea62ed7
Mohan Sahu <mohan@marquee-equity.com>
2025-01-24 16:57:10 +0530

    fix : unexpectedly session expiration

--- backend/middlewares/sessions.js
+++ backend/middlewares/sessions.js
@@ -8,14 +8,14 @@ var s = session({
   "secret": envconfig.vars.mongoStore.secret,
   "cookie": {
     "httpOnly": true,
-    "maxAge": 7 * 24 * 60 * 60 , 
+    "maxAge":  7 * 24 * 60 * 60 * 1000, // in milliseconds  
     // "secure": process.env.PROJECT_ENV == 'prod' ? true :  false
      "secure":    false,
     //  "domain":'https://app.querydeck.io'
   },
   "resave": false,
   "saveUninitialized": false,
-  "rolling": true,
+  // "rolling": true,  // it will refresh token expiration  to new maxage   
   "store": new MongoStore({
     mongoUrl: 'mongodb://mongo:27017/qd_session',
     collection: 'sessions',
